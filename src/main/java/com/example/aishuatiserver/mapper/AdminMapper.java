625f735c9325d2333b6c3a8444e141e3f3b859fb
fy-j <1289757736@qq.com>
2021-12-15 16:06:59 +0800

    Update AdminMapper.java

--- src/main/java/com/example/aishuatiserver/mapper/AdminMapper.java
+++ src/main/java/com/example/aishuatiserver/mapper/AdminMapper.java
@@ -54,7 +54,7 @@ public interface AdminMapper {
     void changeTeacherInfo(AdminInfo adminInfo);
 
     @Select("select Administrator_id as adminId,Administrator_account as adminAccount,Administrator_name as adminName,Administrator_email as adminEmail,Administrator_telephone as adminTelephoto,Administrator_createtime as adminCreateTime,state from administrator where Administrator_id = #{adminId}")
-    public AdminInfo getAdminByAdminId(int adminId);
+    AdminInfo getAdminByAdminId(int adminId);
 
 
 }
